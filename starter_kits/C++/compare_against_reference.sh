d6e3293722be294de1dadaae995183cfb43a55d6
George Guliashvili <george.giorgi.guliashvili@gmail.com>
2019-01-05 00:55:26 +0000

    add 32 56 size boards for benchmarking

--- starter_kits/C++/compare_against_reference.sh
+++ starter_kits/C++/compare_against_reference.sh
@@ -4,4 +4,6 @@ set -e
 make clean
 cmake .
 make
+./halite --replay-directory replays/ -vvv --width 32 --height 32 "./MyBot" "./ReferenceBot"
+./halite --replay-directory replays/ -vvv --width 56 --height 56 "./MyBot" "./ReferenceBot"
 ./halite --replay-directory replays/ -vvv --width 64 --height 64 "./MyBot" "./ReferenceBot"
