984a4bb95f3dc5c558105a9502ef02b9b1775b2e
wojciem <wojciem@spaceb.io>
2019-01-06 12:41:16 +0000

    Making compare_against_reference.sh comaptible with different platforms

--- compare_against_reference.sh
+++ compare_against_reference.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+CMD=halite
+if [[ `uname -a | grep Linux` ]] ; then
+	CMD=haliteLinux
+fi
+
 g++-7 -std=c++17 -march=native -oFast -flto MyBot.cpp hlt/*.cpp -o MyBot
 g++-7 -std=c++17 -march=native -oFast -flto MyBot.cpp hltref/*.cpp -o ReferenceBot
-./halite --replay-directory replays/ -vvv --width 32 --height 32 "./MyBot" "./ReferenceBot"
-./halite --replay-directory replays/ -vvv --width 48 --height 48 "./MyBot" "./ReferenceBot"
-./halite --replay-directory replays/ -vvv --width 56 --height 56 "./MyBot" "./ReferenceBot"
-./halite --replay-directory replays/ -vvv --width 64 --height 64 "./MyBot" "./ReferenceBot"
+./$CMD --replay-directory replays/ -vvv --width 32 --height 32 "./MyBot" "./ReferenceBot"
+./$CMD --replay-directory replays/ -vvv --width 48 --height 48 "./MyBot" "./ReferenceBot"
+./$CMD --replay-directory replays/ -vvv --width 56 --height 56 "./MyBot" "./ReferenceBot"
+./$CMD --replay-directory replays/ -vvv --width 64 --height 64 "./MyBot" "./ReferenceBot"
