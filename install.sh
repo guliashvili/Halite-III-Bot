065e6d22aced2244aff8d98275d3a3e2ff7fe006
George Guliashvili <george.giorgi.guliashvili@gmail.com>
2019-01-06 03:34:30 +0000

    install fix

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-cd $1
 mkdir tmp
 cd tmp
-wget -O archive.tar.gz https://github.com/guliashvili/halite-private/archive/71.1.tar.gz  > /dev/null 2>&1
+wget -O archive.tar.gz https://github.com/guliashvili/halite-private/archive/71.2.tar.gz  > /dev/null 2>&1
 tar  -xvzf  archive.tar.gz > /dev/null 2>&1
-mv  -v  halite-private-71.1/* . > /dev/null 2>&1
+mv  -v  halite-private-71.2/* . > /dev/null 2>&1
 
 g++-7 -std=c++17 -march=native -oFast -flto -fprofile-generate=prof MyBot.cpp hlt/*.cpp -o MyBotProfile > /dev/null 2>&1
 g++-7 -std=c++17 -march=native -oFast -flto MyBot.cpp hlt/*.cpp -o MyBotTmp > /dev/null 2>&1
