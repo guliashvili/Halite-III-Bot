b42656e307a01e02acbbb56c3992089c6eeee19f
George Guliashvili <george.giorgi.guliashvili@gmail.com>
2019-01-06 14:37:44 +0000

    dont use flto. it causes weird bugs

--- installtmp.sh
+++ installtmp.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 
-# pwd
-# mkdir tmp
-# cd tmp
-# wget -O archive.tar.gz https://github.com/guliashvili/halite-private/archive/71.2.tar.gz  > /dev/null 2>&1
-# tar  -xvzf  archive.tar.gz > /dev/null 2>&1
-# mv  -v  halite-private-71.2/* . > /dev/null 2>&1
-
-g++-7 -std=c++17 -march=native -oFast -flto -fprofile-generate=prof MyBot.cpp hlt/*.cpp -o MyBotProfile > /dev/null 2>&1
-g++-7 -std=c++17 -march=native -oFast -flto MyBot.cpp hlt/*.cpp -o MyBotTmp > /dev/null 2>&1
-# g++ -std=c++17 -march=native -oFast -flto -fprofile-generate=prof MyBot.cpp hlt/*.cpp -o MyBotProfile > /dev/null 2>&1
-# g++ -std=c++17 -march=native -oFast -flto MyBot.cpp hlt/*.cpp -o MyBotTmp > /dev/null 2>&1
+g++-7 -std=c++17 -march=native -oFast -fprofile-generate=prof MyBot.cpp hlt/*.cpp -o MyBotProfile > /dev/null 2>&1
+g++-7 -std=c++17 -march=native -oFast MyBot.cpp hlt/*.cpp -o MyBotTmp > /dev/null 2>&1
 
 ./haliteLinux --no-timeout --no-replay --no-logs --width 64 --height 64 "./MyBotProfile" "./MyBotTmp" > /dev/null 2>&1
-# ./halite --no-timeout --no-replay --no-logs --width 64 --height 64 "./MyBotProfile" "./MyBotTmp" > /dev/null 2>&1
 
-# g++-7 -std=c++17 -march=native -oFast -flto  -fprofile-use=prof MyBot.cpp hlt/*.cpp -o MyBot
 g++-7 -std=c++17 -march=native -oFast -fprofile-use=prof MyBot.cpp hlt/*.cpp -o MyBot
-# g++ -std=c++17 -march=native -oFast -flto MyBot.cpp hlt/*.cpp -o MyBot
