043976f18664816c8baaa996466bf741fc13a023
George Guliashvili <george.giorgi.guliashvili@gmail.com>
2019-01-06 03:16:05 +0000

    version 71 auto downloader on host

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 cd $1
-wget -O archive.tar.gz https://github.com/guliashvili/halite-private/archive/68.tar.gz  > /dev/null 2>&1
+wget -O archive.tar.gz https://github.com/guliashvili/halite-private/archive/71.tar.gz  > /dev/null 2>&1
 tar  -xvzf  archive.tar.gz > /dev/null 2>&1
-mv  -v  halite-private-68/starter_kits/C++/* . > /dev/null 2>&1
+mv  -v  halite-private-71/* . > /dev/null 2>&1
 
 g++-7 -std=c++17 -march=native -oFast -flto -fprofile-generate=prof MyBot.cpp hlt/*.cpp -o MyBotProfile > /dev/null 2>&1
 g++-7 -std=c++17 -march=native -oFast -flto MyBot.cpp hlt/*.cpp -o MyBotTmp > /dev/null 2>&1
