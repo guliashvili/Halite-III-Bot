df598d52cdde7da5475cbf1c553165c0bbe5b3c9
George Guliashvili <george.giorgi.guliashvili@gmail.com>
2019-01-06 17:50:58 +0000

    ignore g++ version

--- compare_against_reference.sh
+++ compare_against_reference.sh
@@ -5,8 +5,8 @@ if [[ `uname -a | grep Linux` ]] ; then
 	CMD=haliteLinux
 fi
 
-g++-7 -std=c++17 -march=native -oFast MyBot.cpp hlt/*.cpp -o MyBot
-g++-7 -std=c++17 -march=native -oFast MyBot.cpp hltref/*.cpp -o ReferenceBot
+g++ -std=c++17 -march=native -oFast MyBot.cpp hlt/*.cpp -o MyBot
+g++ -std=c++17 -march=native -oFast MyBot.cpp hltref/*.cpp -o ReferenceBot
 ./$CMD --replay-directory replays/ -vvv --width 32 --height 32 "./MyBot" "./ReferenceBot"
 ./$CMD --replay-directory replays/ -vvv --width 48 --height 48 "./MyBot" "./ReferenceBot"
 ./$CMD --replay-directory replays/ -vvv --width 56 --height 56 "./MyBot" "./ReferenceBot"
